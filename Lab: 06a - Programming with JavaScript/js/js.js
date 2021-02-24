b4f37e6dced0640f6405b0937fced3aeffe2dfc8
Abdullah-AlSawalmeh <abdullahalsawalmeh@gmail.com>
2021-02-24 18:23:22 +0200

    Creation

---
+++ Lab: 06a - Programming with JavaScript/js/js.js
@@ -0,0 +1,22 @@
+
+function stop_repeating () {
+
+
+    for (i = 0; i < 9999; i++) {
+        var ask = prompt ("What is your age? it should be exactly 26, like me");
+        if (ask == 26) {
+            break; 
+        }
+    
+      }
+    
+      var number = prompt ("Are you bored? yes? give a number then "); 
+      for (i = 0; i < number; i++) {
+        document.write('<img src="https://www.todaysparent.com/wp-content/uploads/2017/06/when-your-kid-becomes-a-meme.jpg">');
+    }
+    
+
+
+}
+
+stop_repeating ()
